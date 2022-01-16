3d2493ed0f0de426b205986058f046f03bdfe6ce
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-15 22:42:04 -0500

    WIP: basic setup

---
+++ pages/favorites.js
@@ -0,0 +1,8 @@
+
+export default function favorites() {
+  return (
+    <div>
+      <h1>Hello from favorites page</h1>
+    </div>
+  )
+}
---
+++ pages/search.js
@@ -0,0 +1,8 @@
+
+export default function search() {
+  return (
+    <div>
+      <h1>hello from search page</h1>
+    </div>
+  )
+}
