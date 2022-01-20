b25ac8af125d2a01e8943f216685560b56c76b56
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-20 12:23:39 -0500

    Refactor and clean-up

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import Navigation from "../components/Navigation";
+import axios from "axios";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <Navigation />;
 }
