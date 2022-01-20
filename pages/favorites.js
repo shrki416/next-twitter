f9eeadddffb64fa2d7a3a956af064415c022ef0e
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-20 12:23:12 -0500

    Add favorites page

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,8 +1,33 @@
+import axios from 'axios';
 
-export default function favorites() {
+export default function favorites({ tweets}) {
   return (
     <div>
       <h1>Hello from favorites page</h1>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const URL = `https://api.twitter.com/2/users/44196397/tweets?expansions=author_id&tweet.fields=created_at,author_id,public_metrics&user.fields=username`;
+
+  const headers = {
+    Authorization: `Bearer ${process.env.TOKEN}`,
+  };
+
+  const response = await axios.get(URL, { headers });
+  const tweets = response.data.data;
+  const { users } = response.data.includes;
+
+  tweets.map((tweet) => {
+    const user = users.find((user) => user.id === tweet.author_id);
+    tweet.username = user.username;
+    return tweet;
+  });
+
+  return {
+    props: {
+      tweets,
+    },
+  };
 }
