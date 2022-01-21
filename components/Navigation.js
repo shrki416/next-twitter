16595d651f53129a9515c2ec9f3c6cabf93185c9
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-20 20:12:01 -0500

    Create navigation component

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,159 +1,79 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { CgTwitter } from "react-icons/cg";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 function Navigation() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+   const { theme, setTheme } = useTheme();
+   const [mounted, setMounted] = useState(false);
+
+   useEffect(() => {
+     setMounted(true);
+   }, []);
 
   return (
-    <div className="container mx-auto">
-      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div className="relative flex items-center justify-between">
+    <header className="container mx-auto max-w-2xl">
+      <div className="flex items-center justify-between py-8">
+        <Link href="/">
+          <a className="cursor-pointer ml-5 md:ml-0">
+            <CgTwitter />
+          </a>
+        </Link>
+
+        <div className="hidden md:inline-flex items-center space-x-5">
           <Link href="/">
-            <a
-              aria-label="Company"
-              title="Company"
-              className="inline-flex items-center"
-            >
-              <CgTwitter />
-              <span className="ml-2 text-xl font-bold tracking-wide text-black uppercase">
-                Twitter
-              </span>
+            <a className="font-normal text-gray-600 dark:text-gray-400 hidden md:inline-block p1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all">
+              Home
             </a>
           </Link>
-          <ul className="flex items-center space-x-8 lg:flex md:hidden sm:hidden">
-            <li>
-              <Link href="/">
-                <a
-                  aria-label="landing page"
-                  title="Home page"
-                  className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-teal-accent-400"
-                >
-                  Home
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/search">
-                <a
-                  aria-label="search tweets"
-                  title="Search page"
-                  className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-teal-accent-400"
-                >
-                  Search
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href="/favorite">
-                <a
-                  aria-label="search favorite tweets"
-                  title="Favorite page"
-                  className="font-medium tracking-wide text-black transition-colors duration-200 hover:text-teal-accent-400"
-                >
-                  Favorites
-                </a>
-              </Link>
-            </li>
-          </ul>
-          <div className="lg:hidden">
-            <button
-              aria-label="Open Menu"
-              title="Open Menu"
-              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
-              onClick={() => setIsMenuOpen(true)}
-            >
-              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                />
+          <Link href="/search">
+            <a className="font-normal text-gray-600 dark:text-gray-400 hidden md:inline-block p1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all">
+              Search
+            </a>
+          </Link>
+          <Link href="/favorites">
+            <a className="font-normal text-gray-600 dark:text-gray-400 hidden md:inline-block p1 sm:px-3 sm:py-2 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-800 transition-all">
+              Favorites
+            </a>
+          </Link>
+        </div>
+        <div>
+          <button
+            aria-label="Toggle Dark Mode"
+            type="button"
+            className="w-9 h-9 mr-5 md:mr-0 bg-gray-200 rounded-lg dark:bg-gray-600 flex flex-end items-center justify-center hover:ring-2 ring-gray-300 transition-all"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            {mounted && (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                stroke="currentColor"
+                className="w-5 h-5 text-gray-800 dark:text-gray-200"
+              >
+                {theme === "dark" ? (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                  />
+                ) : (
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                  />
+                )}
               </svg>
-            </button>
-            {isMenuOpen && (
-              <div className="absolute top-0 left-0 w-full">
-                <div className="p-5 bg-white border rounded shadow-sm">
-                  <div className="flex items-center justify-between mb-4">
-                    <div>
-                      <Link href="/">
-                        <a
-                          aria-label="Company"
-                          title="Company"
-                          className="inline-flex items-center"
-                        >
-                          <CgTwitter />
-                        </a>
-                      </Link>
-                    </div>
-                    <div>
-                      <button
-                        aria-label="Close Menu"
-                        title="Close Menu"
-                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                        onClick={() => setIsMenuOpen(false)}
-                      >
-                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                          <path
-                            fill="currentColor"
-                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                  <nav>
-                    <ul className="space-y-4">
-                      <li>
-                        <Link href="/">
-                          <a
-                            aria-label="Home page"
-                            title="Home page"
-                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Home
-                          </a>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/search">
-                          <a
-                            aria-label="search tweets"
-                            title="Search page"
-                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Search
-                          </a>
-                        </Link>
-                      </li>
-                      <li>
-                        <Link href="/favorite">
-                          <a
-                            aria-label="search favorite tweets"
-                            title="Favorite page"
-                            className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                          >
-                            Favorite
-                          </a>
-                        </Link>
-                      </li>
-                    </ul>
-                  </nav>
-                </div>
-              </div>
             )}
-          </div>
+          </button>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
