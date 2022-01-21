283e1c7d9328cb994bba080a8b825bcee0134f5b
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-20 20:13:14 -0500

    Move navigation to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import Navigation from '../components/Navigation';
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider defaultTheme="light" attribute="class">
+      <Navigation />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
