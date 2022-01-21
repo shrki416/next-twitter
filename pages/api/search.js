3a17a234b82541e135df19da92f5a8253e578b8c
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-21 15:56:38 -0500

    WIP: Sent search query to server

--- pages/api/search.js
+++ pages/api/search.js
@@ -1,7 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import axios from 'axios';
+import axios from "axios";
+const USER_END_POINT = `https://api.twitter.com/2/users/by/username/`;
+const TWEETS_END_POINT = `https://api.twitter.com/2/users/`;
+const EXPANSIONS = `?expansions=author_id,attachments.media_keys&tweet.fields=created_at,author_id,public_metrics&user.fields=profile_image_url&media.fields=height,media_key,preview_image_url,type,url,width,alt_text`;
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const headers = {
+  Authorization: `Bearer ${process.env.TOKEN}`,
+};
+
+export default async function handler(req, res) {
+  const query = req.query.q;
+  const response = await axios.get(`${USER_END_POINT}${query}`, { headers });
+  const { id } = response.data.data;
+
+  const tweetsRes = await axios.get(
+    `${TWEETS_END_POINT}${id}/tweets${EXPANSIONS}`,
+    { headers }
+  );
+  const tweets = tweetsRes.data.data;
+  const { users, media } = tweetsRes.data.includes;
+
+  tweets.map((tweet) => {
+    const user = users.find((user) => user.id === tweet.author_id);
+    tweet.username = user.username;
+    tweet.profile_image_url = user.profile_image_url;
+
+    return tweet;
+  });
+
+  res.send(tweets);
 }
