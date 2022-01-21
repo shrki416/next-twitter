a727bb9605c06eb55431f89c30c184f5755ac784
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-21 15:56:57 -0500

    WIP: sending back correct data

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,31 @@
+import Head from "next/head";
+import Link from "next/link";
+import axios from "axios";
+import { useState } from "react";
 
-export default function search() {
+export default function Search() {
+  const [query, setQuery] = useState("");
+
+  const getTweets = async (e) => {
+    e.preventDefault();
+    const { data } = await axios.get(`/api/search?q=${query}`);
+    console.log(`🍏`, data);
+    setQuery("");
+  };
+
   return (
-    <div>
-      <h1>hello from search page</h1>
+    <div className="container mx-auto max-w-2xl">
+      <h1>Search Twitter</h1>
+      <form onSubmit={getTweets}>
+        <input
+          type="text"
+          aria-label="Search"
+          placeholder="Search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button>Send</button>
+      </form>
     </div>
-  )
+  );
 }
