b672ab3359eaf8438e8b55ec8116adc42c4e6bea
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-22 21:21:34 -0500

    Favorites route done

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import Tweet from "../components/tweet";
 import axios from "axios";
+import { useState } from "react";
 
 export async function getStaticProps() {
-  const users = ["tasty", "epicurious", "food52", "seriouseats", "bonappetit"];
+  const users = [
+    "HBHarvest",
+    "epicurious",
+    "food52",
+    "EatingWell",
+    "bonappetit",
+  ];
 
   const BASE_URL = `https://api.twitter.com/2/users/by/username/`;
   const EXPANSIONS = `user.fields=profile_image_url,verified`;
@@ -26,10 +34,12 @@ export async function getStaticProps() {
   };
 }
 
-export default function favorites({ data }) {
+export default function Favorites({ data }) {
+  const [tweets, setTweets] = useState([]);
+
   async function getTweets(e) {
     const { data } = await axios.get(`/api/favorites?id=${e.target.id}`);
-    console.log(data);
+    setTweets(data);
   }
 
   return (
@@ -50,9 +60,12 @@ export default function favorites({ data }) {
           );
         })}
       </div>
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight m-4">
-          Tweets
-        </h1>
+      <h1 className="font-bold text-3xl md:text-5xl tracking-tight m-4">
+        Tweets
+      </h1>
+      {tweets.map((tweet) => (
+        <Tweet key={tweet.id} {...tweet} />
+      ))}
     </div>
   );
 }
