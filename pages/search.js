123857829e2d5bb8987e853c9e2b4ef5efc198b3
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-23 19:26:09 -0500

    Refactor and clean-up

--- pages/search.js
+++ pages/search.js
@@ -12,21 +12,22 @@ export default function Search() {
   const getTweets = async (e) => {
     e.preventDefault();
     const { data } = await axios.get(`/api/search?q=${query}`);
-    console.log(`🚀`, data);
     setTweets(data);
     setQuery("");
   };
 
   return (
     <div className="container mx-auto max-w-2xl">
+      <Head>
+        <title>Search Page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <form
         onSubmit={getTweets}
         className="flex flex-col items-center justify-center rounded p-6"
       >
-        <label
-          className="font-semibold text-lg"
-          htmlFor="Search"
-        >
+        <label className="font-semibold text-lg" htmlFor="Search">
           Search Twitter Username
         </label>
         <input
@@ -37,7 +38,7 @@ export default function Search() {
           onChange={(e) => setQuery(e.target.value)}
           className="flex items-center h-12 px-4 w-64 bg-gray-200 mt-2 rounded focus:outline-none focus:ring-2"
         />
-        <button className='flex items-center justify-center bg-blue-600 rounded text-blue-100 hover:bg-blue-700 h-12 mt-4 w-64'>
+        <button className="flex items-center justify-center bg-blue-600 rounded text-blue-100 hover:bg-blue-700 h-12 mt-4 w-64">
           Send
         </button>
       </form>
