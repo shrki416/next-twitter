15a07932d5b1973f9f877fad2822cc9e1380e0f8
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-24 12:35:25 -0500

    Change home page styles

--- pages/index.js
+++ pages/index.js
@@ -11,18 +11,31 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-5 md:px-20 text-center">
-        <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold text-blue-600">
+        <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold">
           Twitter-Showcase
         </h1>
 
-        <div className="flex flex-wrap items-center max-w-4xl sm:w-full">
-          <Image
-            src='/twitter.png'
-            alt='Twitter'
-            width={800}
-            height={800}
-            className='mx-auto'
-          />
+        <Image src="/twitter.svg" alt="Twitter" width={400} height={400} />
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          <a
+            href="https://github.com/shrki416/next-twitter"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Source Code &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Check out the source code on GitHub, enjoy! 🙂
+            </p>
+          </a>
+
+          <a
+            href="https://developer.twitter.com/en"
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Twitter Documentation &rarr;</h3>
+            <p className="mt-4 text-xl">
+              Learn more about the Twitter API used for this project.
+            </p>
+          </a>
         </div>
       </main>
     </div>
