234bb670a6b685adb4f2ba197ff3f5291c640354
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2022-01-29 21:22:53 -0500

    Follow best practices

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,9 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import Tweet from "../components/Tweet";
+import Favorite from "../components/Favorite";
 import axios from "axios";
-import { useState } from "react";
 
 export async function getStaticProps() {
   const users = [
@@ -35,43 +31,6 @@ export async function getStaticProps() {
   };
 }
 
-export default function Favorites({ data }) {
-  const [tweets, setTweets] = useState([]);
-
-  async function getTweets(e) {
-    const { data } = await axios.get(`/api/favorites?id=${e.target.id}`);
-    setTweets(data);
-  }
-
-  return (
-    <div className="container mx-auto max-w-2xl">
-      <Head>
-        <title>Favorites Page</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <div className="flex justify-around mt-2">
-        {data.map((user) => {
-          return (
-            <button key={user.id} className="pt-4" onClick={getTweets}>
-              <Image
-                className="rounded-full"
-                src={user.profile_image_url}
-                width={50}
-                height={50}
-                alt={user.username}
-                id={user.id}
-              />
-            </button>
-          );
-        })}
-      </div>
-      <h1 className="font-bold text-3xl md:text-5xl tracking-tight m-4">
-        Tweets
-      </h1>
-      {tweets.map((tweet) => (
-        <Tweet key={tweet.id} {...tweet} />
-      ))}
-    </div>
-  );
+export default function favorites({ data }) {
+  return <Favorite data={data} />;
 }
